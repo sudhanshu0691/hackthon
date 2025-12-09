63066161eaec385ca37d585aae4533b6d8f1d8bd
sudhanshu0691 <sudhanshu0691.s@gmail.com>
2025-12-10 02:14:37 +0530

    Use hash-based routing for GitHub Pages compatibility

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
+import { useHashLocation } from "wouter/use-hash-location";
 
-function Router() {
+function AppRouter() {
   return (
     <Switch>
       <Route path="/" component={Home} />
@@ -17,12 +18,14 @@ function Router() {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
+    <Router hook={useHashLocation}>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Toaster />
+          <AppRouter />
+        </TooltipProvider>
+      </QueryClientProvider>
+    </Router>
   );
 }
 
